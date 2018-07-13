71f7735202fb3e5e02d113b41b774cbff40fd98c
yelltian <tianye98@outlook.com>
2018-07-13 15:34:50 +0800

    choose ds

---
+++ ChooseDs/choose_ds.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_BSTButston_clicked()
+{
+
+}
+
+void MainWindow::on_AVLButton_clicked()
+{
+
+}
+
+void MainWindow::on_BPTButton_clicked()
+{
+
+}
